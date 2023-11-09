0babbdf935fee8ce308f55a5e867210888a6595d
jvreca <jure.vreca@ijs.si>
2023-11-09 11:48:12 +0100

    Removed unused tester.

--- src/test/scala/examples/ParameterizedAdderSpecEx.scala
+++ src/test/scala/examples/ParameterizedAdderSpecEx.scala
@@ -20,23 +20,6 @@ class ParameterizedAdderEx[T <: Data, T2 <: Data](gen1: () => T, gen2: () => T2,
   c := RegNext(a.plusContextEx(a1, a2))
 }
 
-class ParameterizedAdderTesterEx[T <: Data: Ring, T2 <: Data](c: ParameterizedAdderEx[T, T2])
-    extends PeekPokeTester(c)
-    with PeekPokeDspExtensions {
-  for {
-    i <- (BigDecimal(-2.0) to 1.0 by 0.25).map(_.toDouble)
-    j <- (BigDecimal(-2.0) to 4.0 by 0.5).map(_.toDouble)
-  } {
-    poke(c.a1, i)
-    poke(c.a2, j)
-    step(1)
-
-    val result = peek(c.c)
-
-    expect(c.c, i + j, s"parameterize adder tester $i + $j => $result should have been ${i + j}")
-  }
-}
-
 class ParameterizedAdderSpecEx extends AnyFlatSpec with ChiselScalatestTester {
 
   behavior.of("parameterized adder circuit with RingEx extension")
